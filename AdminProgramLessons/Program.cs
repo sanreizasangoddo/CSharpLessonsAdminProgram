3f289bc74d6b9d7df8abcc44bb1ff1b58470cbda
Y.Ouallal <2235702@talnet.nl>
2026-03-09 11:56:11 +0100

    Feature Userclass Update

--- AdminProgramLessons/Program.cs
+++ AdminProgramLessons/Program.cs
@@ -11,7 +11,8 @@ namespace AdminProgramLessons
     {
         static void Main(string[] args)
         {
-            User Yassir = new User("Yassir", "Drachtenstraat 4", "+31 6 81994295", "yassir.ouallal@gmail.com", 16, 0217492168);
+            User Yassir = new User("Yassir", "Drachtenstraat 4", "+31 6 81994295", "yassir.ouallal@gmail.com", 16, "lemonke69420");
+            Yassir.ShowInformation();
         }
     }
 }
--- AdminProgramLessons/User.cs
+++ AdminProgramLessons/User.cs
@@ -13,45 +13,39 @@ namespace AdminProgramLessons
         private string _phoneNumber;
         private string _email;
         private int _age;
-        private int _password;
+        private string _password;
 
         public string Name
         {
             get { return _name; }
-            set { _name = value; }
         }
 
         public string Adress
         {
             get { return _adress; }
-            set { _adress = value; }
         }
 
         public string PhoneNumber
         {
             get { return _phoneNumber; }
-            set { _phoneNumber = value; }
         }
 
         public string Email
         {
             get { return _email; }
-            set { _email = value; }
         }
 
         public int Age
         {
             get { return _age; }
-            set { _age = value; }
         }
 
-        public int Password
+        public string Password
         {
             get { return _password; }
-            set { _password = value; }
         }
 
-        public User(string name, string adress, string phoneNumber, string email, int age, int password)
+        public User(string name, string adress, string phoneNumber, string email, int age, string password)
         {
             _name = name;
             _adress = adress;
@@ -59,6 +53,11 @@ namespace AdminProgramLessons
             _email = email;
             _age = age;
             _password = password;
+        }
+
+        public void ShowInformation()
+        {
+            Console.WriteLine($"Welkom {_name}!");
         }
     }
 }
