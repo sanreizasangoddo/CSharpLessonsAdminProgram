fe5897c342f3bbdc59980c052744fa07a18136a6
Y.Ouallal <2235702@talnet.nl>
2026-03-11 11:57:34 +0100

    UserList.cs Updated

--- AdminProgramLessons/Program.cs
+++ AdminProgramLessons/Program.cs
@@ -11,11 +11,18 @@ namespace AdminProgramLessons
     {
         static void Main(string[] args)
         {
-            User Yassir = new User("Yassir", "Drachtenstraat 4", "+31 6 81994295", "yassir.ouallal@gmail.com", 16, "lemonke69420");
-            Yassir.ShowInformation();
+            List<User> userList = new List<User>();
 
-            UserList test = new UserList();
-            test.Start();
+            User Yassir = new User("Yassir", "Drachtenstraat 4", "+31 6 81994925", "yassir.ouallal@gmail.com", 16, "lemonke69420");
+            User Deon = new User("Deon", "de Marken", "+31 6 18217017", "deon.maes@gmail.com", 16, "pizza");
+
+            userList.Add(Yassir);
+            userList.Add(Deon);
+
+            foreach (User user in userList)
+            {
+                Console.WriteLine(user.Name);
+            }
         }
     }
 }
--- AdminProgramLessons/UserList.cs
+++ AdminProgramLessons/UserList.cs
@@ -10,18 +10,7 @@ namespace AdminProgramLessons
     {
         public void Start()
         {
-            List<User> userList = new List<User>();
-
-            User Yassir = new User("Yassir", "Drachtenstraat 4", "+31 6 81994925", "yassir.ouallal@gmail.com", 16, "lemonke69420");
-            User Deon = new User("Deon", "de Marken", "+31 6 18217017", "deon.maes@gmail.com", 16, "pizza");
-
-            userList.Add(Yassir);
-            userList.Add(Deon);
-
-            foreach (User user in userList)
-            {
-                Console.WriteLine(user.Name);
-            }
+            
         }
     }
 }
